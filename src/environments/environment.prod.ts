c4a7d9a6c51556d6e286a1c97936d4cee5a6d40b
Andrew McShane <andrewjohnmcshane@gmail.com>
2020-05-14 03:55:59 -0400

    Add redirect_uri environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  client_id_key: "33ef94537afa40fea1f3f9edc7f68975"
+  client_id_key: "33ef94537afa40fea1f3f9edc7f68975",
+  redirect_uri: "https://spotify-playlist-analyzer-jaja.herokuapp.com/callback"
 };
