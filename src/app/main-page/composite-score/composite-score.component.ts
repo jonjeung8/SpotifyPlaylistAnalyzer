f8ec31c482295ad1088b91c19f3fc678685e7e21
John M. Moore <johnmoore314@gmail.com>
2020-05-15 20:13:47 -0500

    Modify main-page component and composite-score component to refactor code into separations of components.

--- src/app/main-page/composite-score/composite-score.component.ts
+++ src/app/main-page/composite-score/composite-score.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Variable } from '@angular/compiler/src/render3/r3_ast';
 import { MetricScores } from '../../_models/metricScores';
 import { SpotifyApiServiceService } from '../../_services/spotify-api-service.service';
-import { Playlist } from '../../_models/Playlist';
-import { Track } from '../../_models/Track';
-import { Album } from '../../_models/album';
+import { RawMetrics } from '../../_models/RawMetrics';
 
 //imports html and css files
 @Component({ 
@@ -13,27 +10,108 @@ import { Album } from '../../_models/album';
   styleUrls: ['./composite-score.component.css']
 })
 
-
 export class CompositeScoreComponent implements OnInit {
+  //declared variables
+  metricsDisplay: string; //metric selected in dropdown
+  compositeScore: string; //the composite score in string
 
-    //declared variables
-    metricsDisplay: MetricScores;
-    compositeScore: number;
-    apiResponse: string;
-
-    linkSubmitStr : string;
-    bearerTokenStr: string;
-  
-    constructor(private spotifyApi: SpotifyApiServiceService) { 
-
-    //initialize variables
-    this.linkSubmitStr = "";
-    this.metricsDisplay = new MetricScores();
-    this.metricsDisplay.numbers = new Array<number>();
-    this.compositeScore = 0;
-    }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
+  CalculateCompositeScore(arrayOfMetrics: Array<RawMetrics>, categorySelected: string)
+  {
+    //store the element selected
+    this.metricsDisplay = categorySelected;
+    console.log("Hey I made it to calculating composite score");
+    // iterate over the array of metrics
+    // pull the values of the selected metric into a new array to perform stats
+    // we need an average to get started
+    // add outliers here later
+    let total = 0;
+    for (let trackMetric of arrayOfMetrics)
+    {
+      switch(this.metricsDisplay)
+      {
+        case "danceability":
+          {
+            total += trackMetric.danceability;
+            break;
+          }
+        case "energy":
+          {
+            total += trackMetric.energy;
+            break;
+          }
+        case "key":
+          {
+            total += trackMetric.key;
+            break;
+          }
+        case "loudness":
+          {
+            total += trackMetric.loudness;
+            break;
+          }
+        case "mode":
+          {
+            total += trackMetric.mode;
+            break;
+          }
+        case "speechiness":
+          {
+            total += trackMetric.speechiness;
+            break;
+          }
+        case "acousticness":
+          {
+            total += trackMetric.acousticness;
+            break;
+          }
+        case "instrumentalness":
+          {
+            total += trackMetric.instrumentalness;
+            break;
+          }
+        case "liveness":
+          {
+            total += trackMetric.liveness;
+            break;
+          }
+        case "valence":
+          {
+            total += trackMetric.valence;
+            break;
+          }
+        case "tempo":
+          {
+            total += trackMetric.tempo;
+            break;
+          }
+        case "time_signature":
+          {
+            total += trackMetric.time_signature;
+            break;
+          }
+        case "duration_ms":
+          {
+            total += trackMetric.duration_ms;
+            break;
+          }
+        default:
+          {
+            console.log("hello, I've experienced an error somehow, or the user didn't select a category");
+          } 
+      }
+    }
+    console.log(total);
+    console.log(arrayOfMetrics.length);
+    this.compositeScore = parseFloat((total / arrayOfMetrics.length).toFixed(2)).toFixed(2);
+    console.log(this.compositeScore);
+    // console.log(average);
+    // (<HTMLSelectElement>document.getElementById('metricSelected')).value = "0";
+    //TODO: add valueable stats to user here (standard deviation, regression analysis)
+    //TODO: get fix for CORS policy error
+  }
 }
