578d16d1dca7d9f3a2a76f5f773e529d44611678
Jon <jon.jeung@gmail.com>
2020-05-17 18:35:44 -0500

    Initial commit of composite score unit test creation

--- src/app/main-page/composite-score/composite-score.component.spec.ts
+++ src/app/main-page/composite-score/composite-score.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CompositeScoreComponent } from './composite-score.component';
+import { Category } from '../../_models/category';
+import { RawMetrics } from 'src/app/_models/RawMetrics';
 
 describe('CompositeScoreComponent', () => {
   let component: CompositeScoreComponent;
@@ -8,7 +10,7 @@ describe('CompositeScoreComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CompositeScoreComponent ]
+      declarations: [ CompositeScoreComponent]
     })
     .compileComponents();
   }));
@@ -16,10 +18,54 @@ describe('CompositeScoreComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CompositeScoreComponent);
     component = fixture.componentInstance;
+
+    var rawMetrics = new Array<RawMetrics>();
+    
+    for(let i = 0; i < 10; i++)
+    {
+      let tmpMetrics = new RawMetrics();
+      tmpMetrics.acousticness = 1;
+      tmpMetrics.danceability = 2;
+      tmpMetrics.duration_ms = 3;
+      tmpMetrics.energy = 4;
+      tmpMetrics.instrumentalness = 5;
+      tmpMetrics.liveness = 6;
+      tmpMetrics.mode = 7;
+      tmpMetrics.speechiness = 8;
+      tmpMetrics.tempo = 9;
+      tmpMetrics.time_signature = 10;
+      tmpMetrics.valence = 11;
+      this.userPlaylist.metrics.push(tmpMetrics); 
+    }
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should calculate the right average', () => {
+    // ========
+    // Arrange
+    // ========
+    // Array of metrics
+   
+
+    // loop
+    
+
+    // Catergory select string
+    
+    
+    // ========
+    // Act
+    // ========
+
+
+    // ========
+    // Assert
+    // ========
     expect(component).toBeTruthy();
   });
 });
