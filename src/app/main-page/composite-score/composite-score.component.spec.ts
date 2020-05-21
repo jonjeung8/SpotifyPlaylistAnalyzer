174e3f38275f0de137fac369808dd57c7d13472a
Jon <jon.jeung@gmail.com>
2020-05-20 20:37:12 -0500

    Update composite-score.component.spec.ts

--- src/app/main-page/composite-score/composite-score.component.spec.ts
+++ src/app/main-page/composite-score/composite-score.component.spec.ts
@@ -7,6 +7,29 @@ import { RawMetrics } from 'src/app/_models/RawMetrics';
 describe('CompositeScoreComponent', () => {
   let component: CompositeScoreComponent;
   let fixture: ComponentFixture<CompositeScoreComponent>;
+  let rawMetrics: Array<RawMetrics>;
+
+  beforeAll(() => {
+    rawMetrics = new Array<RawMetrics>();
+    
+    for(let i = 0; i < 10; i++)
+    {
+      let tmpMetrics = new RawMetrics();
+      tmpMetrics.acousticness = 1;
+      tmpMetrics.danceability = 2;
+      tmpMetrics.duration_ms = 366000;
+      tmpMetrics.energy = 4;
+      tmpMetrics.instrumentalness = 5;
+      tmpMetrics.liveness = 6;
+      tmpMetrics.mode = 7;
+      tmpMetrics.speechiness = 8;
+      tmpMetrics.tempo = 9;
+      tmpMetrics.time_signature = 10;
+      tmpMetrics.valence = 11;
+      rawMetrics.push(tmpMetrics); 
+    }
+  })
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,53 +42,136 @@ describe('CompositeScoreComponent', () => {
     fixture = TestBed.createComponent(CompositeScoreComponent);
     component = fixture.componentInstance;
 
-    var rawMetrics = new Array<RawMetrics>();
-    
-    for(let i = 0; i < 10; i++)
-    {
-      let tmpMetrics = new RawMetrics();
-      tmpMetrics.acousticness = 1;
-      tmpMetrics.danceability = 2;
-      tmpMetrics.duration_ms = 3;
-      tmpMetrics.energy = 4;
-      tmpMetrics.instrumentalness = 5;
-      tmpMetrics.liveness = 6;
-      tmpMetrics.mode = 7;
-      tmpMetrics.speechiness = 8;
-      tmpMetrics.tempo = 9;
-      tmpMetrics.time_signature = 10;
-      tmpMetrics.valence = 11;
-      this.userPlaylist.metrics.push(tmpMetrics); 
-    }
-
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create composite score component', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should calculate the right average', () => {
-    // ========
+
+  // unit test for calculating average
+  it('should calculate the average acousticness (good data input)', () => {
     // Arrange
-    // ========
-    // Array of metrics
-   
-
-    // loop
+    const metric = 'acousticness';
     
-
-    // Catergory select string
-    
-    
-    // ========
     // Act
-    // ========
+    component.CalculateCompositeScore(rawMetrics, metric);
 
-
-    // ========
     // Assert
-    // ========
-    expect(component).toBeTruthy();
+    expect(component.average).toBe(1);
   });
+
+  it('should calculate the average acousticness (bad data input)', () => {
+    // Arrange
+    const metric = 'key';
+    
+    // Act
+    component.CalculateCompositeScore(rawMetrics, metric);
+
+    // Assert
+    expect(component.average).toBe(0);
+  });
+
+  it('should calculate the average acousticness (null data input)', () => {
+    // Arrange
+    const metric = '0';
+    
+    // Act
+    component.CalculateCompositeScore(rawMetrics, metric);
+
+    // Assert
+    expect(component.average).toBe(0);
+  });
+
+  it('should calculate the average danceability (good data input)', () => {
+    // Arrange
+    const metric = 'danceability';
+    
+    // Act
+    component.CalculateCompositeScore(rawMetrics, metric);
+
+    // Assert
+    expect(component.average).toBe(2);
+  });
+
+  // unite tests for converting metrics to value
+  it('should change the title to acousticness (good input)', () => {
+  // Arrange
+  const metric = 'acousticness';
+    
+  // Act
+  component.CalculateCompositeScore(rawMetrics, metric);
+
+  // Assert
+  expect(component.compositeScoreTitle).toBe('Acousticness rating');
+  });
+
+  it('should change the title to acousticness (bad input)', () => {
+    // Arrange
+    const metric = 'key';
+      
+    // Act
+    component.CalculateCompositeScore(rawMetrics, metric);
+  
+    // Assert
+    expect(component.compositeScoreTitle).toBe(undefined);
+    });
+
+  it('should change the title to acousticness (null input)', () => {
+    // Arrange
+    const metric = '0';
+        
+   // Act
+   component.CalculateCompositeScore(rawMetrics, metric);
+    
+   // Assert
+   expect(component.compositeScoreTitle).toBe(undefined);
+   });
+    
+  //unit tests for valuable data output
+  it('should output value data for acousticness (good input)', () => {
+    // Arrange
+    const metric = 'acousticness';
+      
+    // Act
+    component.CalculateCompositeScore(rawMetrics, metric);
+  
+    // Assert
+    expect(component.compositeScore).toBe('10/10');
+    });
+    
+  it('should output value data for acousticness (bad input)', () => {
+    // Arrange
+    const metric = 'key';
+      
+    // Act
+    component.CalculateCompositeScore(rawMetrics, metric);
+  
+    // Assert
+    expect(component.compositeScore).toBe(undefined);
+    });
+
+  it('should output value data for acousticness (null input)', () => {
+    // Arrange
+    const metric = '0';
+        
+    // Act
+    component.CalculateCompositeScore(rawMetrics, metric);
+    
+    // Assert
+    expect(component.compositeScore).toBe(undefined);
+    });
+
+    it('should output value data for duration (good input)', () => {
+      // Arrange
+      const metric = 'duration_ms';
+        
+      // Act
+      component.CalculateCompositeScore(rawMetrics, metric);
+    
+      // Assert
+      expect(component.compositeScore).toBe('6:06');
+      // expect(component.average).toBe(366000)
+      });
 });
